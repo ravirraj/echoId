0398489fce7b112c2274680e67e71b24f5f4e8e6
yami <ravirrajpatil@gmail.com>
2026-03-03 20:47:34 +0530

    SFTF implemented

--- internal/spectogram/spectrogram.go
+++ internal/spectogram/spectrogram.go
@@ -1,17 +1,23 @@
 package spectogram
 
 import (
+	"fmt"
 	"math"
+
+	"gonum.org/v1/gonum/dsp/fourier"
 )
 
 func GenSpectogram(samples []float64) (int, error) {
 
 	windowSize := 2048
 	hopSize := 512
+	// var result []float64
+	spectogram := [][]float64{}
 
 	frameCount := 0
 	// GenerateHann()
 	hann := GenerateHann(windowSize)
+	fft := fourier.NewFFT(windowSize)
 	for i := 0; i+windowSize <= len(samples); i += hopSize {
 		// fmt.Println(samples[i])
 
@@ -24,12 +30,37 @@ func GenSpectogram(samples []float64) (int, error) {
 		for j := 0; j < windowSize; j++ {
 
 			frameCopy[j] *= hann[j]
+
 			// fmt.Println(frame)
 
 		}
 
+		output := fft.Coefficients(nil, frameCopy)
+
+		n := len(output)
+		mangitudes := make([]float64, n)
+
+		for k := 0; k < n; k++ {
+			realPart := real(output[k])
+			imgPart := imag(output[k])
+
+			mang := realPart*realPart + imgPart*imgPart
+			mangitudes[k] = mang
+			// result = append(result, mang)
+
+		}
+		spectogram = append(spectogram, mangitudes)
+
 		frameCount++
+		// fmt.Println(real(output[0]))
+
 	}
+	fmt.Println(spectogram[0][100])
+	fmt.Println(len(spectogram[0]))
+
+
+	// fmt.Println(result[0])
+	// fmt.Println(real(output[0]))
 
 	return frameCount, nil
 
