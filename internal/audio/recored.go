384f09e37fe14608c88e19b6900ad8760c1c64b6
yami <ravirrajpatil@gmail.com>
2026-03-06 19:50:01 +0530

    record audio in a seperate folder

--- internal/audio/recored.go
+++ internal/audio/recored.go
@@ -1,7 +1,9 @@
 package audio
 
 import (
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 )
 
@@ -15,11 +17,21 @@ func RecordAudio(duration int) (string, error) {
 		"-t", strDuration,
 		"record.wav",
 	)
-	err := cmd.Run()
-	// fmt.Println("Recording...")
+
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	filePath := filepath.Join(dir, "/temp")
+	cmd.Dir = filePath
+
+	_, err = cmd.Output()
 	if err != nil {
 		return "", err
 	}
 
-	return "record.wav", nil
+	// fmt.Println(string(output))
+
+
+	return "temp/record.wav", nil
 }
